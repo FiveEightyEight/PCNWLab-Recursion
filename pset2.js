9a72b58e01bae43d5591b9a57988dc2c07d8d40f
Robert Abreu <robertabreu@pursuit.org>
2018-11-20 23:30:32 -0500

    solution: problem 1 pset2: sum all intergers

--- pset2.js
+++ pset2.js
@@ -1,7 +1,15 @@
 // Sum all integers below a given integer.
 // sumBelow(10); // 45
 // sumBelow(7); // 21
-const sumBelow = n => {};
+const sumBelow = n => {
+    if (n === 0) {
+        return n
+    }
+    return (n-1) + sumBelow(n-1);
+};
+
+console.log(sumBelow(10)); // 45
+console.log(sumBelow(7)); // 21
 
 // Determine if a number is a power of two.
 // powerOfTwo(1); // true
